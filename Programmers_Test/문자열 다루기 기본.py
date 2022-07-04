9224390fdf1bf1b32b78124a217f814ffce86bd3
wooko5 <wooko5@naver.com>
2022-07-05 00:01:02 +0900

    Delete All

--- Programmers_Test/문자열 다루기 기본.py
+++
@@ -1,9 +0,0 @@
-def solution(s):
-    if len(s) == 4 or len(s) == 6:
-        if s.isdigit():
-            return True
-    return False
-
-s = 'a134'
-s = '1234'
-print(solution(s))
--- Programmers_Test/약수더하기.py
+++
@@ -1,6 +0,0 @@
-def solution(n):
-    sum = 0
-    for number in range(1, n+1):
-        if n % number == 0:
-            sum += number
-    return sum
